102b429014433ae5f5955c637bb7de8a1a3276f8
Leandro <leaxavier2014@gmail.com>
2021-10-18 22:57:51 -0500

    subiendo el primer cambio

---
+++ backend-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8026ad51dab388634d331eaf5a39060a'),
+    },
+  },
+});
---
+++ backend-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a581eda6-e241-40d1-b61c-794f26232bf8'
+};
